50857036eb175c1d4e7338897228f811fd599080
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-07 20:43:58 +0200

    LoginPage updated

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,12 +3,6 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-
-import java.time.Duration;
 
 public class LoginPage extends BasePage{
 
